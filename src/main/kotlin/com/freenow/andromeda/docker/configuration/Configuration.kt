48142425f671f3dfc306bff61ef0f595a9f87fb7
Rupin Raveendra Nath <rupinr@live.com>
2020-03-31 12:36:12 +0200

    Status page

--- src/main/kotlin/com/freenow/andromeda/docker/configuration/Configuration.kt
+++ src/main/kotlin/com/freenow/andromeda/docker/configuration/Configuration.kt
@@ -3,6 +3,7 @@ package com.freenow.andromeda.docker.configuration
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
